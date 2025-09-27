2b58a719814e3433f7e6d76237c0677c6a984861
Glazyrin_Andrey <a.glazyrin@ssphera.ru>
2025-09-27 21:48:26 +0300

    create basket logic

--- src/basket/views.py
+++ src/basket/views.py
@@ -3,10 +3,11 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 # Главная
 def basket(request):
     # products = (
     #     Products.objects.select_related("category")
     #     .order_by('?')[:10]
     # )
-    return render(request, "basket.html")
+    return render(request, "basket/basket.html")
